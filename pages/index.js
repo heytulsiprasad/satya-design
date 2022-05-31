f665525c34c2f935e65f48f899205f868d54331d
Tulsi Prasad <tulsi.prasad50@gmail.com>
2022-06-01 01:30:30 +0530

    Update website title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Satyaprakash <a href="https://nextjs.org">Ray</a>
         </h1>
 
         <p className={styles.description}>

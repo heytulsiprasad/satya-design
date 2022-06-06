05538d523181acf6cd15b39b291b7f4fb32e7b1d
Tulsi Prasad <tulsi.prasad50@gmail.com>
2022-06-06 15:58:14 +0530

    Add google analytics to website

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,22 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Head>
           <link
             rel="preload"
